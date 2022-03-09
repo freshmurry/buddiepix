8420a9a7f12192f0e0eb953d27a76de0267730fa
Lawrence <lawrencemurry@yahoo.com>
2022-03-09 10:10:08 +0000

    Trying to get profile upload to work in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "assets/images/:style/default_avatar.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+    
+  has_attached_file :image, service: :s3, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/assets/images/:style/default_avatar.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   
   has_many :posts, dependent: :destroy
   has_many :likes
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   
   validates :name, presence: true, length: {maximum: 50}
   # validates :image, presence: true
-
+  
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email

66b82cb184980f6209f49aac25e6b3e925d8ee08
Lawrence <lawrencemurry@yahoo.com>
2022-03-18 00:20:14 +0000

    Trying to get profile upload to work in production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,53 +1,33 @@
 module ApplicationHelper
-  # def image(user)
-  #   if user.image
-  #     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-  #     "https://www.gravatar.com/avatar/#{gravatar_id}.jpg"
-  #   else  
-  #     return user.image
-  #   end
-  # end
-
-  # def image_url(user)
-  #   if gravatar?(user)
-  #     gravatar = Digest::MD5::hexdigest(user.email).downcase
-  #     # "http://gravatar.com/avatar/#{gravatar}.png?s=200"
-  #     "https://www.gravatar.com/avatar/#{gravatar}.jpg?d=identical&s=150"
-  #   else
-  #     'default_avatar.png'
-  #   end
-  # end
+  def image(user)
+    if user.image
+      'default_avatar.png'
+    elsif
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+      "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+    else
+      def image(user)
+        return user.image
+      end
+    end
+  end
   
-  # private
-  
-  # def add_default_avatar
-  #   unless image.attached?
-  #     avatar.attach(
-  #       io: File.open(
-  #         Rails.root.join(
-  #           'app', 'assets', 'images', 'default_avatar.png'
-  #         )
-  #       ), filename: 'default_avatar.png',
-  #       content_type: 'image/png'
-  #     )
-  #   end
-  # end
-  
-  # def gravatar?(user)
-  #   gravatar = Digest::MD5::hexdigest(user.email).downcase
-  #   gravatar_check = "http://gravatar.com/avatar/#{gravatar}.png?d=404"
-  #   uri = URI.parse(gravatar_check)
-  #   http = Net::HTTP.new(uri.host, uri.port)
-  #   request = Net::HTTP::Get.new(uri.request_uri)
-  #   response = http.request(request)
-  #   response.code.to_i != 404
-  # end
+  def gravatar?(user)
+    gravatar = Digest::MD5::hexdigest(user.email).downcase
+    gravatar_check = "http://gravatar.com/avatar/#{gravatar}.png?d=404"
+    uri = URI.parse(gravatar_check)
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    response.code.to_i != 404
+  end
   
   def @user.website
     link_to @user.website, "#{url_with_protocol(website)}", class: "website", target: :_blank
   end
-  #=> url helper
+  
+  # => url helper
   def url_with_protocol(url)
-      /^http/.match(url) ? url : "http://#{url}"
+    /^http/.match(url) ? url : "http://#{url}"
   end
 end

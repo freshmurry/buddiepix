df810bff992c6caa875d8215b532f4594ee777f3
Lawrence Murry <lawrencemurry@yahoo.com>
2022-03-18 02:19:17 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100#" }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+#   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   attr_accessor :processors
     
   has_many :posts, dependent: :destroy

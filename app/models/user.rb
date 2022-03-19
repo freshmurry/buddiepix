60d0948d740af57f19303c7dd151df6b4fa6326f
Lawrence Murry <lawrencemurry@yahoo.com>
2022-03-18 20:18:41 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[facebook]
   
   validates :name, presence: true, length: {maximum: 50}
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   
-  has_attached_file :image  
   has_many :posts, dependent: :destroy
   has_many :likes
   has_many :comments

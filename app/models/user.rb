ec075803e5d90beba41970386811c64ff0fed661
Lawrence <lawrencemurry@yahoo.com>
2022-03-19 04:40:45 +0000

    Trying to get profile image upload to work in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,9 @@ class User < ApplicationRecord
   
   validates :name, presence: true, length: {maximum: 50}
   validates :image, attachment_presence: true
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "../images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
+  # validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   
   has_many :posts, dependent: :destroy
   has_many :likes

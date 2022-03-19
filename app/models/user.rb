9974fd1067db3448478d7aab79fc96810356728c
Lawrence <lawrencemurry@yahoo.com>
2022-03-19 06:26:15 +0000

    Add a paperclip.rb file to confin/initializers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/images/missing.png"
+  validates_attachment_size :image, :less_than => 5.megabytes
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,9 +10,10 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[facebook]
   
   validates :name, presence: true, length: {maximum: 50}
-  validates :image, attachment_presence: true
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
+  
+  # validates :image, attachment_presence: true
+  # has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
 
   has_many :posts, dependent: :destroy
   has_many :likes
